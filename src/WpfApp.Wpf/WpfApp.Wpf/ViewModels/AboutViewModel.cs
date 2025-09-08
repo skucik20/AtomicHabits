7049456ffb3e8dc891e4371ccc3af92b5f2bd302
skucik20 <skucik20@gmail.com>
2025-09-08 14:57:08 +0200

    Add command to open author's website in About view
    Introduces OpenAuthorSiteCommand to AboutViewModel and binds it to a button in AboutView.xaml, allowing users to open James Clear's website directly from the About page. Cleans up and refactors AboutView layout and text bindings for clarity.

--- src/WpfApp.Wpf/WpfApp.Wpf/ViewModels/AboutViewModel.cs
+++ src/WpfApp.Wpf/WpfApp.Wpf/ViewModels/AboutViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp.Wpf.Helpers.Commands;
 
 namespace WpfApp.Wpf.ViewModels
 {
     public class AboutViewModel
     {
+        public ICommand OpenAuthorSiteCommand { get; set; }
+
+        public AboutViewModel()
+        {
+            OpenAuthorSiteCommand = new RelayCommand(OpenAuthorSite);
+        }
+
+        public void OpenAuthorSite(object parameter)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://jamesclear.com/") { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                // Obsługa błędu, np. brak przeglądarki
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
